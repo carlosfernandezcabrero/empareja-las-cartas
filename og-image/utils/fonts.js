29f05e475950c279a1cc768739819d1671b9f45e
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-04-13 10:07:07 +0200

    move og image generation to Vercel

--- og-image/utils/fonts.js
+++
@@ -1,9 +0,0 @@
-const loadFont = (url) => fetch(url).then((res) => res.arrayBuffer())
-
-export function getFonts(fonts) {
-  const promises = fonts.map((font) => {
-    return loadFont(font)
-  })
-
-  return Promise.all(promises)
-}
---
+++ pages/api/og.tsx
@@ -0,0 +1,62 @@
+import { ImageResponse } from '@vercel/og'
+import type { NextRequest } from 'next/server'
+
+export const config = {
+  runtime: 'edge'
+}
+
+const loadFont = (fontName: string) =>
+  fetch(`https://empareja-las-cartas.vercel.app/fonts/${fontName}.ttf`).then(
+    (res) => res.arrayBuffer()
+  )
+
+export default async function (request: NextRequest) {
+  const { searchParams } = request.nextUrl
+  const text = searchParams.get('text')
+
+  const [fontRegular, fontMedium, fontSemibold, fontBold] = await Promise.all([
+    loadFont('Montserrat-Regular'),
+    loadFont('Montserrat-Medium'),
+    loadFont('Montserrat-SemiBold'),
+    loadFont('Montserrat-Bold')
+  ])
+
+  return new ImageResponse(
+    (
+      <div tw="flex flex-col justify-center items-center w-full h-full">
+        <p tw="text-6xl text-[#112d4e] font-bold">Empareja las cartas</p>
+        <p tw="text-3xl text-gray-600 font-semibold">{text}</p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+      fonts: [
+        {
+          name: 'Montserrat',
+          data: fontRegular,
+          weight: 400,
+          style: 'normal'
+        },
+        {
+          name: 'Montserrat',
+          data: fontMedium,
+          weight: 500,
+          style: 'normal'
+        },
+        {
+          name: 'Montserrat',
+          data: fontSemibold,
+          weight: 600,
+          style: 'normal'
+        },
+        {
+          name: 'Montserrat',
+          data: fontBold,
+          weight: 700,
+          style: 'normal'
+        }
+      ]
+    }
+  )
+}
