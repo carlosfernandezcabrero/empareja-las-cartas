693a8f104a1185a42f08915d2fcb4338f8e8205c
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-04-13 17:07:26 +0200

    add build command

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: process.env.CI ? 'npm run dev' : 'pnpm run dev',
+    command: process.env.CI ? 'npm run start' : 'pnpm run dev',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI
   }
