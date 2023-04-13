c322d64409d38205a50f81630a227ea4fe91744a
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-04-13 08:52:15 +0200

    fix path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../../node_modules/pattern.css/dist/pattern.css'
+import '../node_modules/pattern.css/dist/pattern.css'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
