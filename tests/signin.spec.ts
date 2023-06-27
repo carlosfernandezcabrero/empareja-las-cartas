62ccc4165377012f3758b5937bdeebdde1335820
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-06-27 21:07:03 +0200

    fix tests

--- tests/signin.spec.ts
+++ tests/signin.spec.ts
@@ -5,17 +5,15 @@ test.beforeEach(async ({ page }) => {
 })
 
 test.describe('Signin page tests', () => {
-  test('Should go to levels page when choose play as guess', async ({
-    page
-  }) => {
-    await page.locator('a[href="/menu/level"]').click()
-    await page.waitForURL('/menu/level')
+  test('Should go to game page when choose play as guess', async ({ page }) => {
+    await page.locator('a[href="/game"]').click()
+    await page.waitForURL('/game')
   })
 
   test('Should go to auth0 login when choose login', async ({ page }) => {
     await page.locator('button').click()
     await page.waitForURL(
-      new RegExp(/.*\/api\/auth\/signin\?callbackUrl=%2Fmenu%2Flevel/, 'i')
+      new RegExp(/.*\/api\/auth\/signin\?callbackUrl=%2Fgame/, 'i')
     )
   })
 })
